8fbb2e3708f9f39f82e68304f0e7a6366bd0b8f0
German Baena <germanb66@gmail.com>
2021-09-15 09:12:59 -0500

    Avanzando en los cambios

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 
 const   openModal = document.getElementById('open'),
-        modal = document.getElementById('modal')
+        modal = document.getElementById('modal'),
+        modalContent = document.getElementById('modal-content')
 
 
-openModal.addEventListener('click', () => modal.classList.add('modal--open'))
-
-
-modal.addEventListener('click', e => {
-    if (e.target.classList.contains('modal')) modal.classList.remove('modal--open')
+openModal.addEventListener('click', () => {
+    modal.style.visibility = 'visible'
+    modalContent.classList.add('modal--open')
 })
+
+
+// openModal.addEventListener('click', () =>modalContent.classList.add('modal--open'))
+
+
+// modal.addEventListener('click', e => {
+//     if (e.target.classList.contains('modal')) modal.classList.remove('modal--open')
+// })
